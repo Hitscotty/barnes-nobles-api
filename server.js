41d1223abc759faf6f39a1f54dde62e33a198498
Jonathan <jportorreal77@gmail.com>
2016-12-12 03:16:33 -0500

    fixed port

--- server.js
+++ server.js
@@ -134,6 +134,6 @@ app.get('/genres', (req, res) => {
 
 });
 
-app..listen(process.env.PORT)
-console.log("running on port 3000...");
-
+app.listen(process.env.PORT || 3000, function() {
+    console.log("listening on 3000");
+});
