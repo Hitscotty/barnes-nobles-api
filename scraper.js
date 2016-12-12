f2e7e97a4d26e5e84f14c0327b05c7cfc9120f64
Jonathan <jportorreal77@gmail.com>
2016-12-11 20:14:39 -0500

    subjects endpoint

--- scraper.js
+++ scraper.js
@@ -16,9 +16,13 @@ const fiction = 'www.barnesandnoble.com/b/fiction/_/N-29Z10h8?Ns=P_Sales_Rank';
 
 const app = express();
 
-(function getBiographies(url, pages){
+/* 
+ * scrapes all books on page and repeats by
+ * n number of pages on each visited page
+ */
+(function getBooks(url, pages){
 
-    console.log("scraping barnes and nobles .... ");
+    console.log("### scraping barnes and nobles ###");
 
     book: xr(url, '.resultsListContainer li.clearer > ul > li', [{
 	img: '.product-image img @src',
@@ -29,8 +33,34 @@ const app = express();
 	.paginate('.search-pagination li:last-child a@href')
 	.limit(pages)
 	.write("data.json")
-})
+});
 
+/*
+ * returns all dictionary of all subjects and their unique url
+ */
+function getSubjects(){
+
+    console.log("### scraping subjects of barnes and nobles ###");
+
+    var hash = {};
+    xr('http://www.barnesandnoble.com/h/books/browse', '.links-container.browse.clearer ul li ul li', [{
+	subject: 'a',
+	url: 'a@href',
+    }])((err, obj) => {
+	
+	
+
+	for(var i = 0; i < obj.length; i++) {
+		console.log(obj[i].subject);
+                console.log(obj[i].url);
+	    }
+
+	console.log(hash);
+  })
+	//book.pipe(process.stdout)
+}
+
+getSubjects();
 
 /// API ENDPOINTS ////
 
@@ -38,7 +68,6 @@ app.get('/:genre', (req, res) => {
 
     var genre = req.params.genre;   
     var searchFor = base + genre + sort;
-    console.log(searchFor);
 
 
     var json = xr('http://www.barnesandnoble.com/b/religion/_/N-1fZ29Z17d6?Ns=P_Sales_Rank', '.resultsListContainer li.clearer > ul > li', [{
@@ -52,6 +81,19 @@ app.get('/:genre', (req, res) => {
 	.stream()
 
     json.pipe(res);
+
+});
+
+app.get('/:subjects', (req, res) => {
+
+    var json = xr(url, '.links-container.browse.clearer ul li ul li', [{
+	subject: 'a',
+	url: 'a@href',
+    }])
+	.write()
+
+    json.json(JSON.parse(res));
+
 });
 
 app.listen(3000);
